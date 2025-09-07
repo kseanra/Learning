c2d32f51ee3c2027ebf8b7fb64ff6a5f2478d55b
kliang <kenliangnz@mail.com>
2025-09-07 14:28:56 +1200

    more patterns

--- DesignPatters/CombinePatterns/CombinePatterns/CombineFactorySingletonPattern.cs
+++ DesignPatters/CombinePatterns/CombinePatterns/CombineFactorySingletonPattern.cs
@@ -1,8 +1,10 @@
 // Use case: Combining Factory Method and Singleton for controlled instance creation
 using System;
 public interface IFactorySingletonPattern { void RunExample(); }
-public class FactorySingletonPattern : IFactorySingletonPattern {
-    public void RunExample() {
+public class FactorySingletonPattern : IFactorySingletonPattern
+{
+    public void RunExample()
+    {
         Console.WriteLine("Factory Method + Singleton Pattern Example:");
         var factory = new SingletonFactory();
         var instance1 = factory.GetInstance();
@@ -10,11 +12,14 @@ public class FactorySingletonPattern : IFactorySingletonPattern {
         Console.WriteLine($"Same instance: {ReferenceEquals(instance1, instance2)}");
     }
     class Product { }
-    class SingletonFactory {
+    class SingletonFactory
+    {
         private Product _instance;
-        public Product GetInstance() {
+        public Product GetInstance()
+        {
             if (_instance == null) _instance = new Product();
             return _instance;
         }
     }
 }
+
